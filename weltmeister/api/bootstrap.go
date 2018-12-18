13f3cda950209e0d4e3a40c222bd87f3950604a2
Christian Dreier <dreier.christian@googlemail.com>
2018-12-18 11:59:14 +0100

    bootstrapping impact

--- weltmeister/api/bootstrap.go
+++ weltmeister/api/bootstrap.go
@@ -1,25 +1,48 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"os"
 
 	"github.com/gobuffalo/packd"
+	zglob "github.com/mattn/go-zglob"
 
 	packr "github.com/gobuffalo/packr/v2"
 )
 
 func bootstrapImpact() {
-	// distBox := packr.New("dist", "../../dist")
-	// gameBox := packr.New("dist", "../../game")
-	// mediaBox := packr.New("dist", "../../media")
+
+	matches, err := zglob.Glob(fileRoot + "/**/*.js")
+	if err != nil {
+		log.Fatal("cannot check for empty dir...", err.Error())
+	}
+
+	if len(matches) > 0 {
+		log.Fatal("target folder has already javascript files, to initialize new game, please choose an empty folder")
+	}
+
+	distBox := packr.New("dist", "../../dist")
+	gameBox := packr.New("dist", "../../game")
+	mediaBox := packr.New("dist", "../../media")
 	rootBox := packr.New("dist", "../../rootDump")
 
-	dumpBox(rootBox)
+	dumpBox(rootBox, fileRoot)
+	dumpBox(mediaBox, fileRoot+"media")
+	dumpBox(gameBox, fileRoot+"game")
+	dumpBox(distBox, fileRoot+"dist")
 }
 
-func dumpBox(box *packr.Box) {
-	box.Walk(func(path string, file packd.File) error {
-		log.Println(path)
+func dumpBox(box *packr.Box, folder string) {
+	box.Walk(func(path string, packrFile packd.File) error {
+		absPath := fmt.Sprintf("%s/%s", folder, path)
+		file, err := os.OpenFile(absPath, os.O_CREATE|os.O_RDWR, 0644)
+		if err != nil {
+			log.Println("cannot write", path, err.Error())
+			return err
+		}
+		defer file.Close()
+		file.WriteString(packrFile.String())
 		return nil
 	})
 }
